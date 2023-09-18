5c347b6d5f2c52f0f3a801f51c9a801db8367a39
Anton Rybakov <i@arybakov.ru>
2023-09-18 22:26:19 +0300

    PF index Desk

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,8 @@ const navMain = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.main-nav__toggle');
 
 navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove('main-nav--opened');
+navMain.classList.add('main-nav--closed');
 
 
 navToggle.addEventListener('click', function () {
